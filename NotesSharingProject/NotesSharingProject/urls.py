78c9b94282b844ef0cadde840cae8ef5154bb4ae
niriksha02 <nirikshamanjula@gmail.com>
2021-01-01 19:27:26 +0530

    commited

--- NotesSharingProject/NotesSharingProject/urls.py
+++ NotesSharingProject/NotesSharingProject/urls.py
@@ -16,12 +16,47 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from notes.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about',about,name='about'),
     path('',index,name='index'),
-    path('login',login,name='login'),
-    path('signup',signup,name='signup'),
-    path('login_admin',login_admin,name='login_admin')
-]
+    path('login',userlogin,name='login'),
+    path('signup',signup1,name='signup'),
+    path('admin_home',admin_home,name='admin_home'),
+    path('logout',Logout,name='logout'),
+    path('upload_notes',upload_notes,name='upload_notes'),
+    path('login_admin',login_admin,name='login_admin'),
+    path('profile',profile,name='profile'),
+    path('view_mynotes',view_mynotes,name='view_mynotes'),
+    path('pending_notes',pending_notes,name='pending_notes'),
+    path('accepted_notes',accepted_notes,name='accepted_notes'),
+    path('rejected_notes',rejected_notes,name='rejected_notes'),
+    path('whole_notes',whole_notes,name='whole_notes'),
+    path('view_users',view_users,name='view_users'),
+    path('delete_mynotes/<int:pid>',delete_mynotes,name='delete_mynotes'),
+    path('delete_users/<int:pid>',delete_users,name='delete_users'),
+    path('delete_notes/<int:pid>',delete_notes,name='delete_notes'),
+    path('edit_profile',edit_profile,name="edit_profile"),
+    path('viewallnotes',viewallnotes,name="viewallnotes"),
+    path('changepassword',changepassword,name='changepassword'),
+    path('assign_status/<int:pid>',assign_status,name='assign_status'),
+
+
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- NotesSharingProject/notes/models.py
+++ NotesSharingProject/notes/models.py
@@ -3,22 +3,23 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Signup(models.Model):
 	user=models.ForeignKey(User,on_delete=models.CASCADE)
-	contact=models.CharField(max_len=10,null=True)
-	branch=models.CharField(max_len=30)
-	role=models.CharField(max_len=15)
+	contact=models.CharField(max_length=10,null=True
+		)
+	branch=models.CharField(max_length=30)
+	role=models.CharField(max_length=15)
 
 	def _str_(self):
 		return self.user.username
 
 class Notes(models.Model):
 	user=models.ForeignKey(User,on_delete=models.CASCADE)
-	uploadingdate=models.CharField(max_len=30)
-	branch=models.CharField(max_len=30)
-	subject=models.CharField(max_len=30)
+	uploadingdate=models.CharField(max_length=30)
+	branch=models.CharField(max_length=30)
+	subject=models.CharField(max_length=30)
 	notesfile=models.FileField(null=True)
-	filetype=models.CharField(max_len=30)
-	description=models.CharField(max_len=200,null=True)
-	status=models.CharField(max_len=15)
+	filetype=models.CharField(max_length=30)
+	description=models.CharField(max_length=200,null=True)
+	status=models.CharField(max_length=15)
 	
 
 
